3799e1a404ee39fca31c6f7da9294a9b1f5344d4
vlaskozlov <kozlovvlas@gmail.com>
2023-11-27 23:47:45 +0300

    test

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
 // Обработчик события скролла
+let lastScrollTop = 0;
+
 window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
-    if (window.scrollY > 0) {
+    var currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
         header.style.top = '-80px'; // Скрываем хедер при скролле вниз
     } else {
         header.style.top = '0'; // Показываем хедер при скролле вверх
     }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Определение направления скролла
 });
 
-// Обработчик клика по кнопке меню для мобильных устройств
-document.getElementById('menuButton').addEventListener('click', function() {
-    document.querySelector('.nav').classList.toggle('active');
-    document.getElementById('menuButton').style.display = 'none';
-    document.getElementById('closeMenuButton').style.display = 'block';
-});
-
-// Обработчик клика по кнопке закрытия меню
-document.getElementById('closeMenuButton').addEventListener('click', function() {
-    document.querySelector('.nav').classList.remove('active');
-    document.getElementById('menuButton').style.display = 'block';
-    document.getElementById('closeMenuButton').style.display = 'none';
-});
+//кнопка меню
+$(document).ready(function() {
+    $('.header_burger').click(function(event) {
+        $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+})
