4ee8dc8f2360102613a54d7fb1d30f076a152f7c
D4rkKaizen <dima20050111dima@gmail.com>
2024-02-04 20:33:38 +0200

    final icon update by D4rk

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const menuList = document.querySelectorAll('.header_link')
 	const footer_links = document.querySelector('.links')
 	const copyright = document.documentElement.querySelector('.copyright')
-	const svgElement = document.getElementById('mySvg') // Замените "mySvg" на реальный id вашего SVG элемента
+	const svgElement = document.getElementById('mySvg')
 
 	// Проверяем, включен ли тёмный режим на устройстве пользователя
 	const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
@@ -36,12 +36,21 @@ document.addEventListener('DOMContentLoaded', function () {
 		})
 	}
 
+	// Сохраняем оригинальный код SVG
+	const originalSvgCode = svgElement.innerHTML
+
 	// Добавляем слушатель события для SVG элемента
 	if (svgElement) {
 		svgElement.addEventListener('click', function () {
-			// Здесь можете добавить код для изменения SVG элемента
-			svgElement.innerHTML =
-				'<path d="M479.971-122.5Q331-122.5 226.75-226.779 122.5-331.059 122.5-480.029 122.5-629 226.771-733.25T480-837.5q12 0 24 .75t23.5 2.75q-39.5 29.5-62.75 75t-23.25 99q0 91.042 63.729 154.771Q568.958-441.5 660-441.5q54.065 0 99.282-23.25Q804.5-488 834-527.5q2 11.5 2.75 23.5t.75 24q0 148.958-104.279 253.229Q628.941-122.5 479.971-122.5Zm.192-75Q569-197.5 640-247.75 711-298 744-378.5q-21.333 5.455-42.667 8.727Q680-366.5 660.158-366.5q-122.027 0-207.842-85.816Q366.5-538.131 366.5-660.158q0-19.842 3.273-41.175 3.272-21.334 8.727-42.667-80.5 33-130.75 103.921T197.5-480q0 117.036 82.78 199.768T480.163-197.5ZM471-471Z"/></svg>'
+			// Проверяем, была ли изменена SVG
+			if (svgElement.innerHTML !== originalSvgCode) {
+				// Если изменена, восстанавливаем оригинальный код
+				svgElement.innerHTML = originalSvgCode
+			} else {
+				// Иначе, добавляем код для изменения SVG элемента
+				svgElement.innerHTML =
+					'<path d="M479.971-122.5Q331-122.5 226.75-226.779 122.5-331.059 122.5-480.029 122.5-629 226.771-733.25T480-837.5q12 0 24 .75t23.5 2.75q-39.5 29.5-62.75 75t-23.25 99q0 91.042 63.729 154.771Q568.958-441.5 660-441.5q54.065 0 99.282-23.25Q804.5-488 834-527.5q2 11.5 2.75 23.5t.75 24q0 148.958-104.279 253.229Q628.941-122.5 479.971-122.5Zm.192-75Q569-197.5 640-247.75 711-298 744-378.5q-21.333 5.455-42.667 8.727Q680-366.5 660.158-366.5q-122.027 0-207.842-85.816Q366.5-538.131 366.5-660.158q0-19.842 3.273-41.175 3.272-21.334 8.727-42.667-80.5 33-130.75 103.921T197.5-480q0 117.036 82.78 199.768T480.163-197.5ZM471-471Z"/></svg>'
+			}
 		})
 	}
 })
